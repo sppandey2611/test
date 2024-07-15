7bcb1ca01849a68f19be4fffc51b37cc6225c665
student <shivammpandey11g@gmail.com>
2024-07-16 00:12:43 +0530

    datatype.js

---
+++ datatype.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+// alert(3+3)//we are using node js not browser
+
+console.log(3+3); 
+ 
+let name ="shivam"
+let age =22
+let isLoggedIn =false 
+let state =null 
+
+// number => to power s3
+// bigint 
+// string => true/false 
+//null => standalone value
+// undefined =>
+
+    //symbol => unique
+
+    // object 
+    console.log(typeof age)
+    
